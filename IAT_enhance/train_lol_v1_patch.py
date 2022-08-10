f28613a3ad6eeeb00f25ebba461162e7546ada6a
Cui Ziteng <55396225+cuiziteng@users.noreply.github.com>
2022-08-10 21:17:26 +0800

    Create train_lol_v1_patch.py

---
+++ IAT_enhance/train_lol_v1_patch.py
@@ -0,0 +1,119 @@
+import torch
+import torch.nn as nn
+import torchvision
+import torch.backends.cudnn as cudnn
+import torch.optim
+import torch.nn.functional as F
+
+import os
+import sys
+import argparse
+import time
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+from torchvision.models import vgg16
+
+from data_loaders.lol_v1_new import lowlight_loader_new
+from model.IAT_main import IAT
+
+from IQA_pytorch import SSIM
+from utils import PSNR, adjust_learning_rate, validation, LossNetwork, visualization, CharbonnierLoss
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--gpu_id', type=str, default=0)
+parser.add_argument('--img_path', type=str, default='/data/unagi0/cui_data/light_dataset/LOL_v1/our485_patch/low/')
+parser.add_argument('--img_val_path', type=str, default='/data/unagi0/cui_data/light_dataset/LOL_v1/eval15/low/')
+
+parser.add_argument('--batch_size', type=int, default=8)
+parser.add_argument('--lr', type=float, default=2e-4)
+parser.add_argument('--weight_decay', type=float, default=0.0004)
+parser.add_argument('--pretrain_dir', type=str, default=None)
+
+parser.add_argument('--num_epochs', type=int, default=200)
+parser.add_argument('--display_iter', type=int, default=10)
+parser.add_argument('--snapshots_folder', type=str, default="workdirs/snapshots_folder_lol_v1_patch")
+
+config = parser.parse_args()
+
+print(config)
+os.environ['CUDA_VISIBLE_DEVICES'] = str(config.gpu_id)
+
+if not os.path.exists(config.snapshots_folder):
+    os.makedirs(config.snapshots_folder)
+
+# Model Setting
+model = IAT().cuda()
+if config.pretrain_dir is not None:
+    model.load_state_dict(torch.load(config.pretrain_dir))
+
+# Data Setting
+train_dataset = lowlight_loader_new(images_path=config.img_path)
+train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=config.batch_size, shuffle=True, num_workers=8,
+                                           pin_memory=True)
+val_dataset = lowlight_loader_new(images_path=config.img_val_path, mode='test')
+val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=1, shuffle=False, num_workers=8, pin_memory=True)
+
+optimizer = torch.optim.Adam(model.parameters(), lr=config.lr, betas=(0.9, 0.999), eps=1e-8, weight_decay=config.weight_decay)
+scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=config.num_epochs)
+
+device = next(model.parameters()).device
+print('the device is:', device)
+
+# Loss & Optimizer Setting & Metric
+vgg_model = vgg16(pretrained=True).features[:16]
+vgg_model = vgg_model.cuda()
+
+for param in vgg_model.parameters():
+    param.requires_grad = False
+
+# L1_loss = CharbonnierLoss()
+L1_loss = nn.L1Loss()
+L1_smooth_loss = F.smooth_l1_loss
+
+loss_network = LossNetwork(vgg_model)
+loss_network.eval()
+
+ssim = SSIM()
+psnr = PSNR()
+ssim_high = 0
+psnr_high = 0
+
+model.train()
+print('######## Start IAT Training #########')
+for epoch in range(config.num_epochs):
+    # adjust_learning_rate(optimizer, epoch)
+    print('the epoch is:', epoch)
+    for iteration, imgs in enumerate(train_loader):
+        low_img, high_img = imgs[0].cuda(), imgs[1].cuda()
+        # Checking!
+        #visualization(low_img, 'show/low', iteration)
+        #visualization(high_img, 'show/high', iteration)
+        optimizer.zero_grad()
+        model.train()
+        mul, add, enhance_img = model(low_img)
+
+        loss = L1_loss(enhance_img, high_img)
+        #loss = L1_smooth_loss(enhance_img, high_img)+0.04*loss_network(enhance_img, high_img)
+        loss.backward()
+        
+        optimizer.step()
+        scheduler.step()
+
+        if ((iteration + 1) % config.display_iter) == 0:
+            print("Loss at iteration", iteration + 1, ":", loss.item())
+
+    # Evaluation Model
+    model.eval()
+    PSNR_mean, SSIM_mean = validation(model, val_loader)
+
+    with open(config.snapshots_folder + '/log.txt', 'a+') as f:
+        f.write('epoch' + str(epoch) + ':' + 'the SSIM is' + str(SSIM_mean) + 'the PSNR is' + str(PSNR_mean) + '\n')
+
+    if SSIM_mean > ssim_high:
+        ssim_high = SSIM_mean
+        print('the highest SSIM value is:', str(ssim_high))
+        torch.save(model.state_dict(), os.path.join(config.snapshots_folder, "best_Epoch" + '.pth'))
+
+    f.close()
