68fcde73bcb834105b75c8903d7e36685bda6a54
Cui Ziteng <55396225+cuiziteng@users.noreply.github.com>
2023-03-02 13:51:27 +0800

    Update img_demo.py

--- IAT_enhance/img_demo.py
+++ IAT_enhance/img_demo.py
@@ -3,16 +3,18 @@ import torch
 import cv2
 import argparse
 import warnings
+import torchvision
 import numpy as np
 from utils import PSNR, validation, LossNetwork
 from model.IAT_main import IAT
 from torchvision.transforms import Normalize
 import matplotlib.pyplot as plt
+from PIL import Image
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--file_name', type=str, default='demo_imgs/exposure_demo.JPG')
+parser.add_argument('--file_name', type=str, default='demo_imgs/low_demo.jpg')
 parser.add_argument('--normalize', type=bool, default=False)
-parser.add_argument('--task', type=str, default='exposure', help='Choose from exposure or enhance')
+parser.add_argument('--task', type=str, default='enhance', help='Choose from exposure or enhance')
 config = parser.parse_args()
 
 # Weights path
@@ -33,19 +35,16 @@ model.eval()
 
 
 ## Load Image
-img = plt.imread(config.file_name)
-input = np.asarray(img)
-input = torch.from_numpy(input).float().cuda()
+img = Image.open(config.file_name)
+img = (np.asarray(img)/ 255.0)
+if img.shape[2] == 4:
+    img = img[:,:,:3]
+input = torch.from_numpy(img).float().cuda()
 input = input.permute(2,0,1).unsqueeze(0)
 if config.normalize:    # False
     input = normalize_process(input)
 
 ## Forward Network
 _, _ ,enhanced_img = model(input)
-enhanced_img = enhanced_img.cpu().squeeze(0).permute(1,2,0)
-enhanced_img = enhanced_img.detach().numpy()
-enhanced_img = (enhanced_img*255.0).astype(np.uint8)
 
-plt.imshow(enhanced_img)
-#plt.imsave('results.png', enhanced_img) 
-plt.show()
+torchvision.utils.save_image(enhanced_img, 'result.png')
